852773a0b88ab49e59254a7d2b82c7e488c94b61
Mathias Schütz <schuetz.mathias@outlook.com>
2024-10-08 15:14:25 +0200

    start-tv code now working

---
+++ test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from dbus_next.aio import MessageBus
+
+import asyncio
+import cec
+import subprocess
+import time
+
+# config
+gaming_pc_ip = "192.168.178.64"
+gaming_pc_mac = "D8-43-AE-54-66-8F" # LAN-Adapter MAC
+moonlight_parameters = ['-4k', '-fps 60', '-bitrate 40', '-quitappafter'] # https://github.com/moonlight-stream/moonlight-embedded/wiki/Usage
+
+wake_up_pc_tries = 3
+wake_up_pc_wait_seconds = 10
+
+# wake up pc
+async def wake_up_pc(tries):
+    if (tries == 0):
+        return False
+
+    is_alive = subprocess.call(['ping', '-c', '1', gaming_pc_ip], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0
+    if is_alive:
+        return True
+
+    print(f"Sending wake command to gaming pc. Tries left: {tries-1}")
+    subprocess.Popen('sudo /usr/sbin/etherwake -b ' + gaming_pc_mac.replace("-", ":"), shell=True, stdout=subprocess.PIPE)
+
+    print(f"Wait {wake_up_pc_wait_seconds} sec")
+    await asyncio.sleep(wake_up_pc_wait_seconds)
+
+    return await wake_up_pc(tries-1)
+
+# start tv and connect
+def start_tv():
+    devices = cec.list_devices()
+    devices[0].power_on()
+    cec.set_active_source(devices[0].address)
+    print("TV should be started")
+
+# start moonlight
+def start_moonlight():
+    subprocess.Popen(['moonlight-qt', 'stream'] + moonlight_parameters + [gaming_pc_ip])
+    print("Moonlight started")
