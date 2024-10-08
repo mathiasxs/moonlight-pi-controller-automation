d59dd028282266e84a5143fe7b9efec8b2ef7a18
Mathias Schütz <schuetz.mathias@outlook.com>
2024-10-08 10:14:01 +0200

    optimized

--- controller-automization.py
+++ controller-automization.py
@@ -13,14 +13,14 @@ gaming_pc_mac = "D8-43-AE-54-66-8F" # LAN-Adapter MAC
 moonlight_parameters = ['-4k', '-fps 60', '-bitrate 40', '-quitappafter'] # https://github.com/moonlight-stream/moonlight-embedded/wiki/Usage
 
 wake_up_pc_tries = 3
-wake_up_pc_timeout_seconds = 3
+wake_up_pc_wait_seconds = 3
 
 # init
 cec.init()
 devices = cec.list_devices()
 loop = asyncio.get_event_loop()
 
-async def main():
+async def receive_connected_event():
     bus = await MessageBus().connect()
     introspection = await bus.introspect('org.bluez', "/")
     obj = bus.get_proxy_object('org.bluez', "/", introspection) 
@@ -38,9 +38,9 @@ async def main():
                     print("Error: PC couldn't be woken up")
                     return
 
-                start_moonlight()
                 start_tv()
-                    
+                start_moonlight()
+
     properties.on_properties_changed(device_property_changed_cb)
 
     await loop.create_future()
@@ -49,23 +49,18 @@ async def main():
 async def wake_up_pc(tries):
     if (tries == 0):
         return False
-    
+
     is_alive = subprocess.call(['ping', '-c', '1', gaming_pc_ip], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0
-    if not is_alive:
-        print(f"Sending wake command to gaming pc. Tries left: {tries-1}")
-        subprocess.Popen('sudo /usr/sbin/etherwake -b ' + gaming_pc_mac.replace("-", ":"), shell=True, stdout=subprocess.PIPE)
-
-        print(f"wait {wake_up_pc_timeout_seconds} sec")
-        await asyncio.sleep(wake_up_pc_timeout_seconds)
-
-        return await wake_up_pc(tries-1)
-    else:
+    if is_alive:
         return True
 
-# start moonlight
-def start_moonlight():
-    subprocess.Popen(['moonlight-qt', 'stream'] + moonlight_parameters + [gaming_pc_ip])
-    print("Moonlight started")
+    print(f"Sending wake command to gaming pc. Tries left: {tries-1}")
+    subprocess.Popen('sudo /usr/sbin/etherwake -b ' + gaming_pc_mac.replace("-", ":"), shell=True, stdout=subprocess.PIPE)
+
+    print(f"Wait {wake_up_pc_wait_seconds} sec")
+    await asyncio.sleep(wake_up_pc_wait_seconds)
+
+    return await wake_up_pc(tries-1)
 
 # start tv and connect
 def start_tv():
@@ -76,4 +71,9 @@ def start_tv():
     else:
         print("TV is already on")
 
-loop.run_until_complete(main())
+# start moonlight
+def start_moonlight():
+    subprocess.Popen(['moonlight-qt', 'stream'] + moonlight_parameters + [gaming_pc_ip])
+    print("Moonlight started")
+
+loop.run_until_complete(receive_connected_event())
